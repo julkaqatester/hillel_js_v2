dc00e4d1242654705550d0e7e5496242f7717c6f
Julia <klimuk.julia@gmail.com>
2024-02-28 21:10:28 +0200

    first commit

---
+++ lesson 1/lesson1_hw.js
@@ -0,0 +1,40 @@
+
+/* Task - 1
+Create a variable with the name of our planet.  */
+
+/* Task - 2
+Create a variable to store the name of the current site visitor.*/
+
+/* Task - 3
+Create "var" variable named myAge and set its value equal to 30 */
+
+/* Task - 4
+Create "let" variable named myName and set its value equal to any name */
+
+/* Task - 5
+Create "const" variable named myBool and set its value equal to false */
+
+/* Task - 6
+Display the sum of 15 + 30, using two variables: x and y. */
+
+/* Task - 7
+Create variable named myUndefined and set its value equal to undefined */
+
+/* Task - 8
+Create variable named dividedNumber and set its value equal myAge divided by 2
+ * NOTE: you should use myAge variable here */
+
+/* Task - 9
+Create variable named multipleNumber and set its value equal myAge multiply by 2 */
+
+/* Task - 10
+Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
+NOTE: Result should be something like "Michael is 30"  */
+
+/* Task - 11
+Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage */
+
+/* Task - 12
+Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name. Copy the value from the variable name to admin.
+Print the value of the admin variable. */
+
