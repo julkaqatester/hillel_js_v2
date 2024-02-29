4e8ab1e7cf05cf3a5fe6a78745433b5db66b7e60
Julia <klimuk.julia@gmail.com>
2024-02-29 21:15:22 +0200

    change lesson1 lecture and homework

--- lesson 1/lesson1_hw.js
+++ lesson 1/lesson1_hw.js
@@ -1,40 +1,146 @@
 
 /* Task - 1
-Create a variable with the name of our planet.  */
+Виберіть підходящі імена для змінних та створіть їх в коді.
+        1) Змінна для зберігання дати створеного файла.
+        2) Змінна для зберігання ім'я файлу. 
+        3) Змінна для зберігання кількості помилок. */
 
 /* Task - 2
-Create a variable to store the name of the current site visitor.*/
+        1) Об'явіть дві змінні: login и name
+        2) Змінній name в якості значення привласніть своє ім'я
+        3) Скопіюйте значення із змінної name в login
+        4) Виведіть в консолі  значення змінної login */
 
 /* Task - 3
-Create "var" variable named myAge and set its value equal to 30 */
+        Створіть 3 змінні в одному рядку з різними значеннями */
 
 /* Task - 4
-Create "let" variable named myName and set its value equal to any name */
+        Створіть змінну з іменем myUndefined і встановіть її значення рівним undefined без використання слова "undefined" */
 
 /* Task - 5
-Create "const" variable named myBool and set its value equal to false */
+        Вам потрібно записати рядок "I'm lovin' it!" у змінну resultLine.
+
+        - Значення змінних line1, line2, line3 не змінювати.
+*/
+
+     /*    let line1 = "Lovin";
+        let line2 = "It!";
+        let line3 = "I'm";
+
+        let resultLine = ""; //change this line
+
+        console.log(resultLine); */
 
 /* Task - 6
-Display the sum of 15 + 30, using two variables: x and y. */
+        Вам потрібно записати рядок "70" у змінну digits.
+        Використовуйте змінні x, y, z, порожній рядок та конкатенацію рядків.
+
+        - Значення змінних x, y, z не змінюйте.
+*/
+
+    /* let x = 3;
+    let y = 4;
+    let z = 0;
+
+    let digits =  ""; //change this line
+
+    console.log(digits); */
 
 /* Task - 7
-Create variable named myUndefined and set its value equal to undefined */
+        Вам потрібно записати число 100500 у змінну hugeAmount.
+        Використовуйте змінні bigAmount, greatAmount та перетворення рядка у число.
+        Оголосити та ініціалізувати hugeAmount однією командою.
+
+        - Не змінюйте значення змінних bigAmount і greatAmount.
+        - Використовуйте bigAmount, greatAmount та метод Integer() для ініціалізації змінної hugeAmount.
+*/
+
+
+       /*  let bigAmount = "500";
+        let greatAmount = "100000";
+
+        let hugeAmount = 0; //change this line
+
+        console.log(hugeAmount); */
+
 
 /* Task - 8
-Create variable named dividedNumber and set its value equal myAge divided by 2
- * NOTE: you should use myAge variable here */
+        Змінити код так, щоб замість значень рядків виводилась довжина кожного рядка.
+
+
+        - Не змінюйте значення змінної emptyString.
+        - Для трьох рядків, переданих у метод console.log, слід викликати метод length.
+        - Програма повинна вивести три числа, кожне з нового рядка.
+*/
+
+        /* let emptyString = "";
+
+        console.log(emptyString);
+        console.log("Think Different");
+        console.log((emptyString + 2 + 2 + "22")); */
+
 
 /* Task - 9
-Create variable named multipleNumber and set its value equal myAge multiply by 2 */
+        На екран виводиться 4 рядки. Усі вони є яскравим прикладом зловживання великими літерами.
+        Внесіть зміни у код, щоб усі літери у цих рядках були маленькими.
+
+
+        - Змінні title, degree та career не змінюйте.
+        - Для чотирьох рядків, переданих у метод console.log, слід викликати метод toLowerCase.
+*/
+
+      /*   let title = "Senior Lead Principal Data Analyst";
+        let degree = "In college, I Majored in Economics Science and Minored in Musician Studies.";
+        let career = "Data Scientist With 3+ Years of Experience in Big Data Analytics.";
+
+        console.log("RESUME");
+        console.log("TITLE: " + title);
+        console.log("DEGREE: " + degree);
+        console.log("CAREER: " + career);
+ */
 
 /* Task - 10
-Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
-NOTE: Result should be something like "Michael is 30"  */
+        На екран виводиться 3 рядки. Усі вони є яскравим прикладом зловживання великими літерами.
+        Внесіть зміни у код, щоб усі літери у цих рядках були великими.
+
+        - Значення змінних caps та usa не змінюйте.
+        - Викличте метод toUpperCase для трьох рядків, переданих методу console.log.
+*/
+
+    /* let caps = "if I type in caps ";
+    let fbi = "фбр";
+
+    console.log(fbi);
+    console.log("Гекльберрі Фінн");
+    console.log(caps + "they know I mean business"); */
+
 
 /* Task - 11
-Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage */
+        Додайте операції множення, ділення та віднімання.
+        Відображати результати двома способами: за допомогою конкатенації та за допомогою шаблонних рядків.
+*/
+
+    /*  let inputX = 2;
+        let inputY = 3;
+
+        let x = Number(inputX);
+        let y = Number(inputY);
+
+        //let sum = x + y;
+        let sum = inputX + inputY;
+
+        console.log(`${x} + ${y} = ${sum}`); */
 
 /* Task - 12
-Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name. Copy the value from the variable name to admin.
-Print the value of the admin variable. */
+        Ініціалізуйте змінну message - значення повинно бути створене за домпомогою шаблонного рядка та за допомогою конкотинації
+        Рядкове значення має бути створене на основі значення змінних name, age, company, position, наприклад:
+        Привіт, мене звати Максим. Мені 29 років. Я працюю на посаді QA в компанії Apple.
+*/
 
+    /*  let name = "Максим";
+        let age = 29;
+        let company = "Apple";
+        let position = "QA;
+
+        let message = "";
+        console.log(message); */
