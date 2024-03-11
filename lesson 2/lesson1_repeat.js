28c08e9900316dfaccf5a706caf72563dbbf2726
Julia <klimuk.julia@gmail.com>
2024-03-11 21:16:46 +0200

    add repeated lessons + lesson 4

---
+++ lesson 2/lesson1_repeat.js
@@ -0,0 +1,52 @@
+//Question 0. Яке буде result та typeof result?
+// console.log(3 + 4)
+// console.log('3 + 4')
+// console.log('3' + '4')
+
+//Question 1. Яке буде result та typeof result?
+// const num = 4;
+// const str = "Kovbasa";
+// const result = num + str + num; 
+// console.log(result, typeof result)
+
+//Question 2. Яке буде result та typeof result?
+// const num = 4;
+// const str = 'Playstation';
+// const result = 'Hello' + str + num; 
+// console.log(result, typeof result)
+
+//Question 3. Яке буде result та typeof result?
+// const num = 4;
+// const str = '5' + num;
+// console.log(str, typeof str)
+
+//Question 4. Яке буде result та typeof result?
+// const num = 4;
+// const str = num + '5' + num;
+// console.log(str, typeof str)
+
+//Question 5. Яке буде result та typeof result?
+/* let name = "Rom";
+let count = (name + 12).length;
+console.log(count) */
+
+//Question 6**. Яке буде result та typeof result?
+/* let num = 5;
+let numb = num + num + '1' + num;
+console.log(numb) */
+
+
+
+
+
+
+
+
+
+//Порядок виконання: ((a + a) + "1") + a = 1015
+
+
+
+
+
+
---
+++ lesson 3/lesson2_repeat.js
@@ -0,0 +1,56 @@
+// Question 1. Який оператор порівняння використовує switch?
+
+// Question 2. Яке ключове слово зупиняє процес порівняння в switch?
+
+// Question 3. Чи буде показано console log?
+if ("0") {
+  console.log('Привіт');
+}
+
+// Question 4. Яке значення у с?
+let e = 2; let b = 1; let c;
+console.log(c);
+
+// Question 5. Який буде результат цих виразів?
+console.log(5 > 4)
+console.log("ананас" > "яблуко")
+console.log("2" > "12")
+console.log(undefined == null)
+console.log(undefined === null )
+
+// Question 6. Які кінцеві значення всіх змінних a та x після виконання коду нижче?
+let a = 2;
+let x = 1 + (a * 2);
+//let x = 1 + (a *= 2);
+console.log("a = " + a)
+console.log("x = " + x)
+
+// Question 7. Які кінцеві значення всіх змінних q, r, w та d після виконання коду нижче?
+let q = 1; 
+let r = 1;
+let w = ++q;
+let d = r++;
+
+console.log("q = " + q)
+console.log("r = " + r)
+console.log("w = " + w)
+console.log("d = " + d)
+
+
+// Question 8. Приклад завдання додому. Які результати цих виразів?
+console.log("" + 1 + 0)
+console.log("" - 1 + 0)
+console.log("" + 1 - 2)
+console.log(true + false)
+console.log(6 / "3")
+console.log("2" * "3")
+console.log(4 + 5 + "px")
+console.log("px" + 4 + 5 )
+console.log("$" + 4 + 5)
+console.log("4" - 2)
+console.log("4px" - 2)
+console.log("  -9  " + 5)
+console.log("  -9  " - 5)
+console.log(null + 1)
+console.log(undefined + 1)
+console.log(" \t \n" - 2)
