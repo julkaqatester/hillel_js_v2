9f643d856aeb6ce378e01aa71a8cd71ac9bc322a
Julia <klimuk.julia@gmail.com>
2024-03-18 19:45:31 +0200

    add lesson6

---
+++ lesson6_repetition/lesson6_group1.js
@@ -0,0 +1,21 @@
+let str = "JavaScript";
+let letter = str[1]; 
+console.log(letter);
+
+let text = "Джаваскріпт - це весело";
+let length = text.length;
+console.log(length);
+
+var result = 0/0;
+console.log(nan)
+
+var guessWho = 3.14;
+guessWho = 'А тепер тут строка';
+guessWho = false;
+guessWho = 9.14;
+console.log(guessWho)
+
+const num = 42;
+const str1 = "Shuba";
+const result = num + str1; 
+console.log(result)
---
+++ lesson6_repetition/lesson6_group2.js
@@ -0,0 +1,22 @@
+let str = "DataTypes";
+let letter = str[1]; 
+console.log(letter);
+
+let text = "Джаваскріпт - це цікаво";
+let length = text.length;
+console.log(length);
+
+var result = "qwerty"/0;
+console.log(nan)
+
+
+var guessWho = 3.14;
+guessWho = 'А тепер тут строка';
+guessWho = false;
+guessWho = 8.97;
+console.log(guessWho)
+
+const num = 68;
+const str1 = "Borsh";
+const result = num + str1; 
+console.log(result)
